1549a509977ab65bc65742a93e7a9f55d10599c7
Patrick Arthur <patricksilva08@hotmail.com>
2021-02-25 14:47:07 -0300

    script base

---
+++ _script/script.js
@@ -0,0 +1,44 @@
+// SCROLL SUAVE
+
+function initScrollSuave(){
+
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block:'start'
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+initScrollSuave();
+
+// ANIMAÇÃO AO SCROLL
+
+function initAnimacaoScroll(){
+
+    const sections = document.querySelectorAll('.js-scroll');
+        if(sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+
+        function animaScroll () {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - windowMetade;
+                if(sectionTop < 0) {
+                    section.classList.add('ativo');
+                }
+            });
+        }
+        animaScroll();
+        window.addEventListener('scroll', animaScroll);
+        }
+}
+initAnimacaoScroll();
